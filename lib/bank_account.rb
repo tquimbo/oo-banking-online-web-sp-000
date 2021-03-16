55f6d8a2c095ab77744a21feeebb68a7126b1f32
Tim Quimbo <tquimbo1@gmail.com>
2021-03-16 01:05:39 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,8 @@
+require 'pry'
+
 class BankAccount
 
-  attr_accessor :name, :balance, :status
+  attr_accessor :name, :balance, :status, :amount
 
   def initialize(name)
     @name = name
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,30 +1,36 @@
+require 'pry'
 class Transfer
 
   attr_accessor :transfer, :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
-    transaction
     @sender = sender
     @receiver = receiver
     @amount = amount
-  end
-
-  def transaction
-    @transfer = transfer
     @status = 'pending'
   end
 
   def valid?
-
+    @sender.valid? && @receiver.valid?
   end
 
   def execute_transaction
-
-  end
+     if valid? && @status = "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = 'complete'
+      elsif !valid?
+      @status = 'rejected'
+      return "Transaction rejected. Please check your account balance."
+     end
+   end
 
   def reverse_transfer
-
+    if @status == 'complete'
+      @sender.deposit += @amount
+      @receiver.deposit -= @amount
+      @status == 'reversed'
+    end
   end
 
-  
 end
